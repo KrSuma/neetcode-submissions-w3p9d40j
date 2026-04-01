97a5ae18c5ed1b356b64cbdbe8f479b694ef4c3c
Jae Sun Lee <jaesun90@gmail.com>
2026-04-01 13:16:47 +0900

    Add: simplify-path - submission-3

---
+++ Data Structures & Algorithms/simplify-path/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        
+        stack = []
+        res = ''
+
+        for c in path + '/':
+            if c == '/': 
+                if res == '..':
+                    if stack: stack.pop()
+                elif res != '.' and res != '':
+                    stack.append(res)
+                res = ''
+            else:
+                res += c
+
+        return '/' + '/'.join(stack)
