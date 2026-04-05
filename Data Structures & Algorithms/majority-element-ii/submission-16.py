cc0bcfc3d3c8c1a8971ad529835f1978c654c759
Jae Sun Lee <jaesun90@gmail.com>
2026-04-05 15:09:15 +0900

    Add: majority-element-ii - submission-16

---
+++ Data Structures & Algorithms/majority-element-ii/submission-16.py
@@ -0,0 +1,23 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        
+        hashmap = defaultdict(int)
+
+        for n in nums:
+            hashmap[n] += 1
+
+            if len(hashmap) <= 2:
+                continue
+            
+            temp = defaultdict(int)
+            for k, v in hashmap.items():
+                if v > 1:
+                    temp[k] = v - 1
+            hashmap = temp
+
+        res = []
+        for n in hashmap:
+            if nums.count(n) > len(nums) / 3:
+                res.append(n)
+        
+        return res
