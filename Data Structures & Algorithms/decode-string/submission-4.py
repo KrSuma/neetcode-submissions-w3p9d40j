edfd660cb515c5c689ffc4601b09dfb492f8c058
Jae Sun Lee <jaesun90@gmail.com>
2026-04-06 17:56:40 +0900

    Add: decode-string - submission-4

---
+++ Data Structures & Algorithms/decode-string/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        stack = []
+
+        for c in s:
+            res = ''
+            if c == ']':
+                while stack[-1] != '[':
+                    res = stack.pop() + res
+                stack.pop()
+
+                digit = ''
+                while stack and stack[-1].isdigit():
+                    digit = stack.pop() + digit
+                stack.append(int(digit) * res)
+            else:
+                stack.append(c)
+
+        return ''.join(stack)
