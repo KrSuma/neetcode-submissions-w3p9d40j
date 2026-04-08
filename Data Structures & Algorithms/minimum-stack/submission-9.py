65982a82a8fa301649ef0ea2f0fb6df8bdd1cd5a
Jae Sun Lee <jaesun90@gmail.com>
2026-04-08 10:00:59 +0900

    Add: minimum-stack - submission-9

---
+++ Data Structures & Algorithms/minimum-stack/submission-9.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.s = []
+        self.minstack = []
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+        if not self.minstack:
+            self.minstack.append(val)
+        else:
+            if self.minstack[-1] < val:
+                self.minstack.append(self.minstack[-1])
+            else:
+                self.minstack.append(val)
+
+    def pop(self) -> None:
+        self.s.pop()
+        self.minstack.pop()
+
+    def top(self) -> int:
+        return self.s[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
